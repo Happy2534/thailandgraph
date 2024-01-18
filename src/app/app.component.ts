8ace7cca7c581f001b8a87d8cbcbee2e9757a2d5
HappyWtLz <happynaja7@gmail.com>
2024-01-18 14:00:30 +0700

    first comment test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts/highmaps';
+import ExportingModule from 'highcharts/modules/exporting';
+import SunsetTheme from 'highcharts/themes/sunset.js';
+
+ExportingModule(Highcharts);
+SunsetTheme(Highcharts);
+Highcharts.setOptions({
+  title: {
+    style: {
+      color: 'tomato'
+    }
+  },
+  legend: {
+    enabled: false
+  }
+});
 
 @Component({
   selector: 'app-root',
@@ -6,5 +22,14 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isHighcharts = typeof Highcharts === 'object';
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    series: [{
+      data: [1, 2, 3],
+      type: 'line'
+    }]
+  };
+
   title = 'thailandgraph';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
+
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
+import * as Highcharts from 'highcharts';
+import HC_map from 'highcharts/modules/map';
+import { ThailandmapChartComponent } from './thailandmap-chart/thailandmap-chart.component';
+HC_map(Highcharts);
+// AppRoutingModule,
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThailandmapChartComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [HttpClientModule,
+    BrowserModule,FormsModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
